e092f4b3820a34ebb668fd528b6860c316827d9b
yvz <oguzcanyavuz@hotmail.com>
2018-04-19 17:37:17 +0300

    Now we are printing results when a page is fetched.

--- search_jobs.js
+++ search_jobs.js
@@ -22,58 +22,54 @@ function get_ordinal(n) {
    return n + (s[(v - 20) % 10] || s[v] || s[0]);
 }
 
-function args_handler() {
+async function args_handler() {
     let location = argv.l, radius = argv.r, keyword = argv.k, api_key = default_api_key;
     if(argv.api)
         api_key = argv.api;
     // start the program if the given args are correct
-    get_company_ids(api_key, location, radius, keyword).then((company_infos) => {
-        console.log(company_infos);
-        console.log("Total companies found: " + company_infos.length);
-    });
+    // TODO: main'deki uyariyi coz. kodu daha modueler hale getir.
+    main(api_key, location, radius, keyword);
 }
 
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function get_company_ids(api_key, location, radius, keyword) {
+async function get_company_ids(api_key, location, radius, keyword, page_token, counter) {
     return new Promise(async function(resolve) {
-        let company_infos = [];
-        let page_token = "";
-        let counter = 1;
-        while(page_token !== undefined) {
-            let options = {
-                uri: "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
-                qs: {
-                    location: location,
-                    radius: radius,
-                    keyword: keyword,
-                    key: api_key,
-                    pagetoken: page_token
-                },
-                resolveWithFullResponse: true
-            };
-            await rp(options)
-                .then(function(response) {
-                    response = JSON.parse(response.body);
-                    if(!response)
-                        return Promise.reject(new Error('fail')).catch(() => console.err);
-                    console.log("Fetching " + get_ordinal(counter++) + " page.");
-                    let results = response.results;
-                    for(let i in results) {
-                        if(results.hasOwnProperty(i)) {
-                            get_company_infos(results[i].place_id, api_key).then((result) => {
-                                if(result.website !== undefined)     // check if the website link exist
-                                    company_infos.push(result);
-                            });
-                        }
+        let company_infos = {
+            results: [],
+            page_token: undefined
+        };
+        let options = {
+            uri: "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
+            qs: {
+                location: location,
+                radius: radius,
+                keyword: keyword,
+                key: api_key,
+                pagetoken: page_token
+            },
+            resolveWithFullResponse: true
+        };
+        await rp(options)
+            .then(function(response) {
+                response = JSON.parse(response.body);
+                if(!response)
+                    return Promise.reject(new Error('fail')).catch(() => console.err);
+                console.log("Fetching " + get_ordinal(counter) + " page.");
+                let results = response.results;
+                for(let i in results) {
+                    if(results.hasOwnProperty(i)) {
+                        get_company_infos(results[i].place_id, api_key).then((result) => {
+                            if(result.website !== undefined)     // check if the website link exist
+                                company_infos.results.push(result);
+                        });
                     }
-                    page_token = response.next_page_token;
-                })
-                .catch(console.error);
-            await timeout(2000);
-        }
+                }
+                company_infos.page_token = response.next_page_token;
+            });
+        await timeout(2000);
         return resolve(company_infos);
     });
 }
@@ -102,7 +98,21 @@ async function get_company_infos(company_id, api_key) {
                 result[keys[i]] = company_details[i];
             return result;
         })
-        .catch(console.error);
+}
+
+async function main(api_key, location, radius, keyword) {
+    let page_token = "";
+    let company_counter = 0;
+    let counter = 1;
+    while(page_token !== undefined) {
+        await get_company_ids(api_key, location, radius, keyword, page_token, counter).then((company_infos) => {
+            console.log(company_infos.results);
+            page_token = company_infos.page_token;
+            company_counter += company_infos.results.length;
+            counter++;
+        }).catch(console.error);
+    }
+    console.log("Total companies found: " + company_counter);
 }
 
 args_handler();
